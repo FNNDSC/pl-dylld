b1d9502aa10028dd469237772380ecbd89a0a9a2
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-01-06 14:45:11 -0500

    Additional CLI for orthanc/CUBE, better env support, refactoring

--- dylld.py
+++ dylld.py
@@ -13,7 +13,11 @@ from    pudb.remote             import set_trace
 
 from    loguru                  import logger
 from    concurrent.futures      import ThreadPoolExecutor
+from    threading               import current_thread
+
 from    typing                  import Callable
+
+from    datetime                import datetime, timezone
 
 from    state                   import data
 from    logic                   import behavior
@@ -21,12 +25,12 @@ from    control                 import action
 from    control.filter          import PathFilter
 
 
-Env             = data.CUBEinstance()
+Env             = data.env()
 PLinputFilter   = None
 LOG             = None
 LLD             = None
 
-__version__ = '1.0.13'
+__version__ = '3.2.0'
 
 DISPLAY_TITLE = r"""
        _           _       _ _     _ 
@@ -76,7 +80,12 @@ parser.add_argument(
 parser.add_argument(
             '--orthancIP',
             default = 'https://orthanc-chris-public.apps.ocp-prod.massopen.cloud/',
-            help    = 'Orthanc to receive analysis results'
+            help    = 'IP of the orthanc to receive analysis results'
+)
+parser.add_argument(
+            '--orthancport',
+            default = '',
+            help    = 'Orthanc DICOM port'
 )
 parser.add_argument(
             '--orthancuser',
@@ -103,13 +112,38 @@ parser.add_argument(
             help    = "use threading to branch in parallel",
             dest    = 'thread',
             action  = 'store_true',
-            default = False)
+            default = False
+)
 parser.add_argument(
             "--inNode",
-            help    = "perform in-node implicit parallelization",
+            help    = "perform in-node implicit parallelization in conjunction with --thread",
             dest    = 'inNode',
             action  = 'store_true',
-            default = False)
+            default = False
+)
+parser.add_argument(
+            "--debug",
+            help    = "if true, toggle telnet pudb debugging",
+            dest    = 'debug',
+            action  = 'store_true',
+            default = False
+)
+parser.add_argument(
+            "--debugTermSize",
+            help    = "the terminal 'cols,rows' size for debugging",
+            default = '253,62'
+)
+parser.add_argument(
+            "--debugPort",
+            help    = "the debugging telnet port",
+            default = '7900'
+)
+parser.add_argument(
+            "--debugHost",
+            help    = "the debugging telnet host",
+            default = '0.0.0.0'
+)
+
 
 def _mapper_dir_contains_factory(glob: str) -> Callable[[Path], bool]:
     """
@@ -126,19 +160,40 @@ def _mapper_dir_contains_factory(glob: str) -> Callable[[Path], bool]:
 
     return _dir_contains
 
+def Env_setup(options: Namespace, inputdir: Path, outputdir: Path):
+    """
+    Setup the environment
+
+    Args:
+        options (Namespace):    options passed from the CLI caller
+        inputdir (Path):        plugin global input directory
+        outputdir (Path):       plugin global output directory
+    """
+    global Env
+    Env.CUBE.inputdir       = str(inputdir)
+    Env.CUBE.outputdir      = str(outputdir)
+    Env.CUBE.port           = str(options.CUBEport)
+    Env.CUBE.IP             = str(options.CUBEIP)
+    Env.orthanc.IP          = str(options.orthancIP)
+    Env.orthanc.port        = str(options.orthancport)
+    Env.orthanc.username    = str(options.orthancuser)
+    Env.orthanc.password    = str(options.orthancpassword)
+    Env.inputdir            = inputdir
+    Env.outputdir           = outputdir
+    Env.debug_setup(    debug       = options.debug,
+                        termsize    = options.debugTermSize,
+                        port        = options.debugPort,
+                        host        = options.debugHost
+    )
 
 def ground_prep(options: Namespace, inputdir: Path, outputdir: Path):
     '''
     Perform some setup and initial LOG output
     '''
-    global Env, LOG, LLD, PLinputFilter
-    Env.inputdir        = str(inputdir)
-    Env.outputdir       = str(outputdir)
-    Env.port            = str(options.CUBEport)
-    Env.IP              = str(options.CUBEIP)
+    global Env, LOG, PLinputFilter
 
-    PLinputFilter       = action.PluginRun(     env = Env, options = options)
-    LOG                 = logger.debug
+    PLinputFilter           = action.PluginRun(env = Env, options = options)
+    LOG                     = logger.debug
 
     LOG("Starting growth cycle...")
 
@@ -156,9 +211,10 @@ def ground_prep(options: Namespace, inputdir: Path, outputdir: Path):
     LOG("outputdir = %s" % str(outputdir))
 
     if len(options.pluginInstanceID):
-        Env.parentPluginInstanceID  = options.pluginInstanceID
+        Env.CUBE.parentPluginInstanceID  = options.pluginInstanceID
     else:
-        Env.parentPluginInstanceID  = Env.parentPluginInstanceID_discover()['parentPluginInstanceID']
+        Env.CUBE.parentPluginInstanceID  = \
+            Env.CUBE.parentPluginInstanceID_discover()['parentPluginInstanceID']
 
 def tree_grow(options: Namespace, input: Path, output: Path = None) -> dict:
     '''
@@ -168,23 +224,33 @@ def tree_grow(options: Namespace, input: Path, output: Path = None) -> dict:
     '''
     global Env, PLinputFilter, LLD, LOG
 
-    pudb.set_trace()
+    Env.set_trace()
 
+    timenow                 = lambda: datetime.now(timezone.utc).astimezone().isoformat()
     LLD                     = action.LLDcomputeflow(env = Env, options = options)
     conditional             = behavior.Filter()
     conditional.obj_pass    = behavior.unconditionalPass
 
-    LOG("Growing a tree off new data root %s..." % str(input))
+    str_threadName          : str   = current_thread().getName()
+    LOG("Growing a tree in thread %s..." % str_threadName)
+    str_heartbeat           = str(Env.outputdir.joinpath('heartbeat-%s.log' % str_threadName))
+    fl                      = open(str_heartbeat, 'w')
+    fl.write('Start time: {}'.format(timenow()))
     if conditional.obj_pass(str(input)):
         LOG("Tree planted off %s" % str(input))
         d_nodeInput         = PLinputFilter(str(input))
         if d_nodeInput['status']:
             d_LLDflow       = LLD(  d_nodeInput['branchInstanceID'])
+            LOG('Flow result:')
+            LOG('%s' % json.dumps(d_LLDflow, indent = 4))
+            LOG('-30-')
         else:
             LOG("Some error was returned from the node analysis!",  comms = 'error')
             LOG('stdout: %s' % d_nodeInput['run']['stdout'],        comms = 'error')
             LOG('stderr: %s' % d_nodeInput['run']['stderr'],        comms = 'error')
             LOG('return: %s' % d_nodeInput['run']['returncode'],    comms = 'error')
+    fl.write('End   time: {}'.format(timenow()))
+    fl.close()
 
 # documentation: https://fnndsc.github.io/chris_plugin/chris_plugin.html#chris_plugin
 @chris_plugin(
@@ -202,13 +268,16 @@ def main(options: Namespace, inputdir: Path, outputdir: Path):
     :param outputdir: directory where to write output files
     """
 
+    global Env, LOG
+    # set_trace(term_size=(253, 62), host = '0.0.0.0', port = 7900)
+
+    Env_setup(options, inputdir, outputdir)
+    Env.set_telnet_trace_if_specified()
+
     print(DISPLAY_TITLE)
 
-    set_trace(term_size=(253, 62), host = '0.0.0.0', port = 7900)
-
-    global Env, PFMlogger, LOG, CAW, PLinputFilter
     ground_prep(options, inputdir, outputdir)
-    if len(Env.parentPluginInstanceID):
+    if len(Env.CUBE.parentPluginInstanceID):
         LOG("Sewing seeds...")
         Path('%s/start.touch' % str(outputdir)).touch()
         output = None
