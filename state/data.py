b67ad28ab3c4d836d7c065929a29c51514cd006b
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-01-06 14:44:11 -0500

    Add env class for CUBE, orthanc, and debug meta-data

--- state/data.py
+++ state/data.py
@@ -6,8 +6,10 @@ str_about = '''
     information relevant to the pipeline to be scheduled.
 '''
 
-from    curses      import meta
-from    pathlib     import Path
+from    pudb.remote             import set_trace
+from    curses                  import meta
+from    pathlib                 import Path
+import  pudb
 import  json
 import  os
 
@@ -21,6 +23,88 @@ class env:
         '''
         Constructor
         '''
+        self._inputdir  : Path              = None
+        self._outputdir : Path              = None
+        self.CUBE       : CUBEinstance      = CUBEinstance()
+        self.orthanc    : Orthancinstance   = Orthancinstance()
+        self.debug      : dict              = {
+            'do'        : False,
+            'termsize'  : (80,25),
+            'port'      : 7900,
+            'host'      : '0.0.0.0'
+        }
+
+    def debug_setup(self, **kwargs) -> dict:
+        """
+        Setup the debugging structure based on <kwargs>
+
+        Returns:
+            dict: the debug structure
+        """
+        str_termsize    : str   = ""
+        str_port        : str   = ""
+        str_host        : str   = "0.0.0.0"
+        b_debug         : bool  = False
+        for k,v in kwargs.items():
+            if k == 'debug'     :   b_debug         = v
+            if k == 'termsize'  :   str_termsize    = v
+            if k == 'port'      :   str_port        = v
+            if k == 'host'      :   str_host        = v
+
+        cols, rows  = str_termsize.split(',')
+        self.debug['do']        = b_debug
+        self.debug['termsize']  = (int(cols), int(rows))
+        self.debug['port']      = int(str_port)
+        self.debug['host']      = str_host
+        return self.debug
+
+    def set_telnet_trace_if_specified(self):
+        """
+        If specified in the env, pause for a telnet debug.
+
+        If you are debugging, just "step" to return to the location
+        in your code where you specified to break!
+        """
+        if self.debug['do']:
+            set_trace(
+                term_size   = self.debug['termsize'],
+                host        = self.debug['host'],
+                port        = self.debug['port']
+            )
+
+    def set_trace(self):
+        """
+        Simple "override" for setting a trace. If the Env is configured
+        for debugging, then this set_trace will be called. Otherwise it
+        will be skipped.
+
+        This is useful for leaving debugging set_traces in the code, and
+        being able to at runtime choose to debug or not.
+
+        If you are debugging, just "step" to return to the location
+        in your code where you specified to break!
+
+        Returns:
+            _type_: _description_
+        """
+        if self.debug['do']:
+            pudb.set_trace()
+
+    @property
+    def inputdir(self):
+        return self._inputdir
+
+    @inputdir.setter
+    def inputdir(self, a):
+        self._inputdir = a
+
+    @property
+    def outputdir(self):
+        return self._outputdir
+
+    @outputdir.setter
+    def outputdir(self, a):
+        self._outputdir = a
 
 class CUBEinstance:
     '''
@@ -160,35 +244,39 @@ class Orthancinstance:
 
     def __init__(self, *args, **kwargs):
         self.d_orthanc = {
-            'user'      : 'chris',
-            'password'  : 'chris1234',
-            'address'   : '192.168.1.200',
-            'remote'    : ''
+            'username'  : 'orthanc',
+            'password'  : 'orthanc',
+            'IP'        : '192.168.1.200',
+            'port'      : '4242',
+            'remote'    : '',
+            'protocol'  : 'http',
+            'route'     : '',
+            'url'       : ''
         }
 
     @property
-    def address(self):
-        return self.d_orthanc['address']
+    def IP(self):
+        return self.d_orthanc['IP']
 
-    @address.setter
-    def address(self, a):
-        self.d_orthanc['address'] = a
+    @IP.setter
+    def IP(self, a):
+        self.d_orthanc['IP'] = a
 
     @property
     def port(self):
-        return self.d_CUBE['port']
+        return self.d_orthanc['port']
 
     @port.setter
     def port(self, a):
-        self.d_CUBE['port'] = a
+        self.d_orthanc['port'] = a
 
     @property
     def username(self):
-        return self.d_orthanc['user']
+        return self.d_orthanc['username']
 
     @username.setter
     def username(self, a):
-        self.d_orthanc['user'] = a
+        self.d_orthanc['username'] = a
 
     @property
     def password(self):
@@ -208,14 +296,14 @@ class Orthancinstance:
 
     def user(self, *args) -> str:
         '''
-        get/set the CUBE user
+        get/set the orthanc username
         '''
-        if len(args): self.d_orthanc['user']  = args[0]
-        return self.d_orthanc['user']
+        if len(args): self.d_orthanc['username']  = args[0]
+        return self.d_orthanc['username']
 
     def password(self, *args) -> str:
         '''
-        get/set the CUBE user
+        get/set the orthanc password
         '''
         if len(args): self.d_orthanc['password']  = args[0]
         return self.d_orthanc['password']
@@ -235,6 +323,25 @@ class Orthancinstance:
             return self.d_orthanc[str_key]
         else:
             return ''
+
+    def url(self, *args):
+        '''
+        get/set the URL
+        '''
+        str_colon : str = ""
+        if len(self.d_orthanc['port']):
+            str_colon   = ":"
+        if len(args):
+            self.d_orthanc['url']  = args[0]
+        else:
+            self.d_orthanc['url']  = '%s://%s%s%s%s' % (
+                self.d_orthanc['protocol'],
+                self.d_orthanc['IP'],
+                str_colon,
+                self.d_orthanc['port'],
+                self.d_orthanc['route']
+            )
+        return self.d_orthanc['url']
 
 class Pipeline:
     '''
