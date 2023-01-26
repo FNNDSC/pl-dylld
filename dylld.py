f0b5bd3d68e668cfb05ba4857604b70509132374
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-01-26 15:27:24 -0500

    Significant refactoring to remove single global variable for critical per-thread variables, bump to 4.0.0

--- dylld.py
+++ dylld.py
@@ -7,17 +7,19 @@ from    chris_plugin            import chris_plugin, PathMapper
 
 from    pathlib                 import Path
 
+from    io                      import TextIOWrapper
 import  os, sys
 import  pudb
 from    pudb.remote             import set_trace
 
 from    loguru                  import logger
-from    concurrent.futures      import ThreadPoolExecutor
-from    threading               import current_thread
+from    concurrent.futures      import ThreadPoolExecutor, ProcessPoolExecutor
+from    threading               import current_thread, get_native_id
 
 from    typing                  import Callable
 
 from    datetime                import datetime, timezone
+import  json
 
 from    state                   import data
 from    logic                   import behavior
@@ -25,26 +27,38 @@ from    control                 import action
 from    control.filter          import PathFilter
 
 
-Env             = data.env()
-PLinputFilter   = None
-LOG             = None
-LLD             = None
+LOG             = logger.debug
 
-__version__ = '3.4.8'
+logger_format = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> │ "
+    "<level>{level: <5}</level> │ "
+    "<yellow>{name: >28}</yellow>::"
+    "<cyan>{function: <30}</cyan> @"
+    "<cyan>{line: <4}</cyan> ║ "
+    "<level>{message}</level>"
+)
+logger.remove()
+logger.add(sys.stderr, format=logger_format)
+
+pluginInputDir:Path     = None
+pluginOutputDir:Path    = None
+ld_forestResult:list    = []
+
+__version__ = '4.0.0'
 
 DISPLAY_TITLE = r"""
-       _           _       _ _     _ 
+       _           _       _ _     _
       | |         | |     | | |   | |
  _ __ | |______ __| |_   _| | | __| |
 | '_ \| |______/ _` | | | | | |/ _` |
 | |_) | |     | (_| | |_| | | | (_| |
 | .__/|_|      \__,_|\__, |_|_|\__,_|
-| |                   __/ |          
-|_|                  |___/           
+| |                   __/ |
+|_|                  |___/
 """
 
 
-parser          = ArgumentParser(
+parser: ArgumentParser      = ArgumentParser(
     description = '''
 A ChRIS plugin that dynamically builds a workflow to compute length
 discrepencies from extremity X-Rays
@@ -144,32 +158,23 @@ parser.add_argument(
             default = '0.0.0.0'
 )
 
-
-def _mapper_dir_contains_factory(glob: str) -> Callable[[Path], bool]:
+def Env_setup(  options         : Namespace,
+                inputdir        : Path,
+                outputdir       : Path,
+                debugPortOffset : int = 0) -> data.env:
     """
-    Creates a function suitable for use with a ``PathMapper``.
-    That function returns true if its path argument is a directory
-    containing a file which matches the given glob.
-    """
-
-    def _dir_contains(path: Path) -> bool:
-        if not path.is_dir():
-            return False
-        match = path.glob(glob)
-        return next(match, None) is not None
-
-    return _dir_contains
-
-def Env_setup(options: Namespace, inputdir: Path, outputdir: Path):
-    """
-    Setup the environment
+   Setup the environment
 
     Args:
         options (Namespace):    options passed from the CLI caller
         inputdir (Path):        plugin global input directory
         outputdir (Path):       plugin global output directory
+        debugPortOffset (int, optional): offset added to debug port -- useful for multithreading. Defaults to 0.
+
+    Returns:
+        data.env: _description_
     """
-    global Env
+    Env: data.env           = data.env()
     Env.CUBE.inputdir       = str(inputdir)
     Env.CUBE.outputdir      = str(outputdir)
     Env.CUBE.url            = str(options.CUBEurl)
@@ -182,20 +187,17 @@ def Env_setup(options: Namespace, inputdir: Path, outputdir: Path):
     Env.outputdir           = outputdir
     Env.debug_setup(    debug       = options.debug,
                         termsize    = options.debugTermSize,
-                        port        = options.debugPort,
+                        port        = int(options.debugPort) + debugPortOffset,
                         host        = options.debugHost
     )
+    return Env
 
-def ground_prep(options: Namespace, inputdir: Path, outputdir: Path):
-    '''
-    Perform some setup and initial LOG output
-    '''
-    global Env, LOG, PLinputFilter
+def preamble_show(options: Namespace) -> None:
+    """
+    Just show some preamble "noise" in the output terminal
+    """
 
-    PLinputFilter           = action.PluginRun(env = Env, options = options)
-    LOG                     = logger.debug
-
-    LOG("Starting growth cycle...")
+    print(DISPLAY_TITLE)
 
     LOG("plugin arguments...")
     for k,v in options.__dict__.items():
@@ -207,50 +209,115 @@ def ground_prep(options: Namespace, inputdir: Path, outputdir: Path):
          LOG("%25s:  [%s]" % (k, v))
     LOG("")
 
-    LOG("inputdir  = %s" % str(inputdir))
-    LOG("outputdir = %s" % str(outputdir))
+    LOG("Starting growth cycle...")
+
+def ground_prep(options: Namespace, Env : data.env) -> action.PluginRun:
+    """
+    Do some per-tree setup -- prepare the ground!
+
+    Args:
+        options (Namespace): options namespace
+        Env (data.env): the environment for this tree
+
+    Returns:
+        action.PluginRun: A filter specific to this tree that will
+                          filter a study of interest in the parent
+                          space -- analogously akin to choosing a
+                          seed.
+    """
+
+    LOG("Prepping ground for tree in thread %s..." % get_native_id())
+    LOG("Constructing object to filter parent field")
+    PLinputFilter: action.PluginRun = action.PluginRun(env = Env, options = options)
 
     if len(options.pluginInstanceID):
         Env.CUBE.parentPluginInstanceID  = options.pluginInstanceID
     else:
         Env.CUBE.parentPluginInstanceID  = \
             Env.CUBE.parentPluginInstanceID_discover()['parentPluginInstanceID']
+    return PLinputFilter
 
 def tree_grow(options: Namespace, input: Path, output: Path = None) -> dict:
-    '''
-    Based on some conditional of the <input> direct the
-    dynamic "growth" of this feed tree from the parent node
-    of *this* plugin.
-    '''
-    global Env, PLinputFilter, LLD, LOG
+    """
+    Based on some conditional applied to the <input> file space, direct the
+    dynamic "growth" of this feed tree from the parent node of *this* plugin.
 
-    Env.set_trace()
+    Args:
+        options (Namespace): CLI options
+        input (Path): input path returned by mapper
+        output (Path, optional): ouptut path returned by mapper. Defaults to None.
 
-    timenow                 = lambda: datetime.now(timezone.utc).astimezone().isoformat()
-    LLD                     = action.LLDcomputeflow(env = Env, options = options)
-    conditional             = behavior.Filter()
-    conditional.obj_pass    = behavior.unconditionalPass
+    Returns:
+        dict: resulant object dictionary of this (threaded) growth
+    """
+    global pluginInputDir, pluginOutputDir, LOG, ld_forestResult
 
-    str_threadName          : str   = current_thread().getName()
-    LOG("Growing a tree in thread %s..." % str_threadName)
-    str_heartbeat           = str(Env.outputdir.joinpath('heartbeat-%s.log' % str_threadName))
-    fl                      = open(str_heartbeat, 'w')
-    fl.write('Start time: {}'.format(timenow()))
+    Env:data.env = Env_setup(
+                        options,
+                        pluginInputDir,
+                        pluginOutputDir,
+                        get_native_id()
+                    )
+    Env.set_telnet_trace_if_specified()
+
+    timenow:Callable[[], str]       = lambda: datetime.now(timezone.utc).astimezone().isoformat()
+    conditional:behavior.Filter     = behavior.Filter()
+    conditional.obj_pass            = behavior.unconditionalPass
+    PLinputFilter:action.PluginRun  = ground_prep(options, Env)
+    LLD:action.LLDcomputeflow       = action.LLDcomputeflow(env = Env, options = options)
+    str_threadName:str              = current_thread().getName()
+    d_seedGet:dict                  = {
+        "status"    : False,
+        "message"   : "unable to plant seed"
+    }
+    d_treeGrow:dict                 = {
+        "status"    : False,
+        "message"   : "unable to grow tree"
+    }
+    d_ret:dict                      = {
+        "seed"      : {},
+        "tree"      : {}
+    }
+
+    Path('%s/start-%s.touch' % (Env.outputdir.touch(), str_threadName))
+    LOG("Growing a new tree in thread %s..." % str_threadName)
+    str_heartbeat:str               = str(Env.outputdir.joinpath('heartbeat-%s.log' % \
+                                            str_threadName))
+    fl:TextIOWrapper                = open(str_heartbeat, 'w')
+    fl.write('Start time: {}\n'.format(timenow()))
     if conditional.obj_pass(str(input)):
-        LOG("Tree planted off %s" % str(input))
-        d_nodeInput         = PLinputFilter(str(input))
-        if d_nodeInput['status']:
-            d_LLDflow       = LLD(  d_nodeInput['branchInstanceID'])
+        LOG("Planting seed off %s" % str(input))
+        d_seedGet                   = PLinputFilter(str(input))
+        if d_seedGet['status']:
+            d_treeGrow              = LLD(d_seedGet['branchInstanceID'])
             LOG('Flow result:')
-            LOG('%s' % json.dumps(d_LLDflow, indent = 4))
+            LOG('%s' % json.dumps(d_treeGrow, indent = 4))
             LOG('-30-')
         else:
-            LOG("Some error was returned from the node analysis!",  comms = 'error')
-            LOG('stdout: %s' % d_nodeInput['run']['stdout'],        comms = 'error')
-            LOG('stderr: %s' % d_nodeInput['run']['stderr'],        comms = 'error')
-            LOG('return: %s' % d_nodeInput['run']['returncode'],    comms = 'error')
-    fl.write('End   time: {}'.format(timenow()))
+            LOG("Some error was returned when planting the seed!")
+            LOG('stdout: %s' % d_seedGet['run']['stdout'])
+            LOG('stderr: %s' % d_seedGet['run']['stderr'])
+            LOG('return: %s' % d_seedGet['run']['returncode'])
+    fl.write('End   time: {}\n'.format(timenow()))
     fl.close()
+    d_ret['seed']       = d_seedGet
+    d_ret['tree']       = d_treeGrow
+    ld_forestResult.append(d_ret)
+    return d_ret
+
+def treeGrowth_savelog(outputdir : Path):
+    """
+    Write the global log file on the tree growth to the passed
+    <outputdir>
+
+    Args:
+        outputdir (Path): the plugin base output directory
+    """
+    global ld_forestResult
+
+    with open(str(outputdir.joinpath('treeLog.json')), 'w') as f:
+        f.write(json.dumps(ld_forestResult, indent=4))
+    f.close()
 
 # documentation: https://fnndsc.github.io/chris_plugin/chris_plugin.html#chris_plugin
 @chris_plugin(
@@ -267,40 +334,35 @@ def main(options: Namespace, inputdir: Path, outputdir: Path):
     :param inputdir: directory containing input files (read-only)
     :param outputdir: directory where to write output files
     """
+    global pluginInputDir, pluginOutputDir
+    pluginInputDir  = inputdir
+    pluginOutputDir = outputdir
 
-    global Env, LOG
-    # set_trace(term_size=(253, 62), host = '0.0.0.0', port = 7900)
+    set_trace(term_size=(253, 62), host = '0.0.0.0', port = 7900)
 
-    Env_setup(options, inputdir, outputdir)
-    Env.set_telnet_trace_if_specified()
+    preamble_show(options)
 
-    print(DISPLAY_TITLE)
-
-    ground_prep(options, inputdir, outputdir)
-    if len(Env.CUBE.parentPluginInstanceID):
-        LOG("Sewing seeds...")
-        Path('%s/start.touch' % str(outputdir)).touch()
-        output = None
-
-    LOG("Sewing seeds...")
-    Path('%s/start.touch' % str(outputdir)).touch()
-    output = None
-    # Are we processing all the data in one tree (i.e. inNode)
-    # or will every data element have its own tree?
+    output: None = None
     if not options.inNode:
-        mapper  = PathMapper.file_mapper(inputdir, outputdir,
-                            glob        = options.pattern)
+        mapper  = PathMapper.file_mapper(
+                            inputdir,
+                            outputdir,
+                            glob        = options.pattern
+                        )
     else:
-        mapper  = PathMapper(inputdir, outputdir,
-                            filter      = _mapper_dir_contains_factory(options.pattern))
+        mapper  = PathMapper.dir_mapper_deep(
+                            inputdir,
+                            outputdir
+                        )
     if int(options.thread):
-        with ThreadPoolExecutor(max_workers=len(os.sched_getaffinity(0))) as pool:
+        with ProcessPoolExecutor(max_workers=len(os.sched_getaffinity(0))) as pool:
             results = pool.map(lambda t: tree_grow(options, *t), mapper)
     else:
         for input, output in mapper:
-            tree_grow(options, input, output)
+            results: dict = tree_grow(options, input, output)
 
     LOG("Ending growth cycle...")
+    treeGrowth_savelog(outputdir)
 
 if __name__ == '__main__':
     main()
