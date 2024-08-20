d427cd8dd7d516ff26385cc38979959a111b0ec7
Sandip Samal <sandip.samal@childrens.harvard.edu>
2024-08-20 11:40:53 -0400

    version lock chrisclient

--- dylld.py
+++ dylld.py
@@ -46,7 +46,7 @@ pluginInputDir:Path
 pluginOutputDir:Path
 ld_forestResult:list    = []
 
-__version__ = '4.4.36'
+__version__ = '4.4.38'
 
 DISPLAY_TITLE = r"""
        _           _       _ _     _
